f625a97e22bd83a58acf08ed6df6b00e68577535
A Devipriya <83152452+Devipriya1921@users.noreply.github.com>
2022-09-15 19:28:48 +0530

    vsdbabysoc.tcl file

--- vsdbabysoc.tcl
+++ vsdbabysoc.tcl
@@ -1,15 +1,15 @@
-set target_library [list /home/skandha/Tools_SNPS/Downloads_temp/SAED32_EDK/lib/stdcell_hvt/db_ccs/saed32hvt_tt0p85v25c.db /home/aakash/VSDBabySoC/src/lib/avsdpll.db  /home/aakash/VSDBabySoC/src/lib/avsddac.db]
-set link_library [list  /home/skandha/Tools_SNPS/Downloads_temp/SAED32_EDK/lib/stdcell_hvt/db_ccs/saed32hvt_tt0p85v25c.db  /home/aakash/VSDBabySoC/src/lib/avsdpll.db  /home/aakash/VSDBabySoC/src/lib/avsddac.db] 
+set target_library [list /home/skandha/Tools_SNPS/Downloads_temp/SAED32_EDK/lib/stdcell_hvt/db_ccs/saed32hvt_tt0p85v25c.db /home/devipriya/VSDBabySoC/src/lib/avsdpll.db  /home/devipriya/VSDBabySoC/src/lib/avsddac.db]
+set link_library [list  /home/skandha/Tools_SNPS/Downloads_temp/SAED32_EDK/lib/stdcell_hvt/db_ccs/saed32hvt_tt0p85v25c.db  /home/devipriya/VSDBabySoC/src/lib/avsdpll.db  /home/devipriya/VSDBabySoC/src/lib/avsddac.db] 
 set symbol_library ""
 
-read_file -format verilog {/home/aakash/VSDBabySoC/src/module/vsdbabysoc.v}
-read_verilog /home/aakash/VSDBabySoC/src/module/rvmyth.v
-read_verilog /home/aakash/VSDBabySoC/src/module/clk_gate.v
+read_file -format verilog {/home/devipriya/VSDBabySoC/src/module/vsdbabysoc.v}
+read_verilog /home/devipriya/VSDBabySoC/src/module/rvmyth.v
+read_verilog /home/devipriya/VSDBabySoC/src/module/clk_gate.v
 
-read_lib -lib /home/aakash/VSDBabySoC/src/module/avsdpll.lib
-read_lib -lib /home/aakash/VSDBabySoC/src/module/avsddac.lib
+read_lib -lib /home/devipriya/VSDBabySoC/src/module/avsdpll.lib
+read_lib -lib /home/devipriya/VSDBabySoC/src/module/avsddac.lib
 
-analyze -library WORK -format verilog {/home/aakash/VSDBabySoC/src/module/vsdbabysoc.v}
+analyze -library WORK -format verilog {/home/devipriya/VSDBabySoC/src/module/vsdbabysoc.v}
 elaborate vsdbabysoc -architecture verilog -library WORK
 analyze {}
 
@@ -53,11 +53,11 @@ check_design
 compile_ultra
 
 file mkdir report
-write -hierarchy -format verilog -output /home/aakash/VSDBabySoC/src/module/report/vsdbabysoc_gtlvl.v
-write_sdc -nosplit -version 2.0 /home/aakash/VSDBabySoC/src/module/report/vsdbabysoc.sdc
-report_area -hierarchy > /home/aakash/VSDBabySoC/src/module/report/vsdbabysoc.area
-report_timing > /home/aakash/VSDBabySoC/src/module/report/vsdbabysoc.timing
-report_power -hierarchy > /home/aakash/VSDBabySoC/src/module/report/vsdbabysoc.power
+write -hierarchy -format verilog -output /home/devipriya/VSDBabySoC/src/module/report/vsdbabysoc_gtlvl.v
+write_sdc -nosplit -version 2.0 /home/devipriya/VSDBabySoC/src/module/report/vsdbabysoc.sdc
+report_area -hierarchy > /home/devipriya/VSDBabySoC/src/module/report/vsdbabysoc.area
+report_timing > /home/devipriya/VSDBabySoC/src/module/report/vsdbabysoc.timing
+report_power -hierarchy > /home/devipriya/VSDBabySoC/src/module/report/vsdbabysoc.power
 
 gui_start
 
