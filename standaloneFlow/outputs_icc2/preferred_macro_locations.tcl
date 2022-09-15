cb61afa8a5a148401079d16b2ecbd56f7664af6c
A Devipriya <83152452+Devipriya1921@users.noreply.github.com>
2022-09-15 14:51:00 +0530

    files

---
+++ standaloneFlow/outputs_icc2/preferred_macro_locations.tcl
@@ -0,0 +1,4 @@
+#set_macro_constraints -preferred_location {0.211 0.218} [get_cells sram]
+#set_macro_constraints -preferred_location {0.211 0.617} [get_cells vsdbabysoc]
+
+
---
+++ standaloneFlow/outputs_icc2/preferred_port_locations.tcl
@@ -0,0 +1,15 @@
+START PHYSICAL PIN CONSTRAINTS;
+    {pins OUT[9]} {reference vsdbabysoc_1} {layers metal2} {sides 4} {offset 45.2200};
+    {pins OUT[8]} {reference vsdbabysoc_1} {layers metal2} {sides 4} {offset 133.7600};
+    {pins OUT[7]} {reference vsdbabysoc_1} {layers metal2} {sides 4} {offset 222.3000};
+    {pins OUT[6]} {reference vsdbabysoc_1} {layers metal3} {sides 3} {offset 45.7800};
+    {pins OUT[5]} {reference vsdbabysoc_1} {layers metal3} {sides 3} {offset 134.4000};
+    {pins OUT[4]} {reference vsdbabysoc_1} {layers metal3} {sides 3} {offset 223.0200};
+    {pins OUT[3]} {reference vsdbabysoc_1} {layers metal2} {sides 2} {offset 222.4900};
+    {pins OUT[2]} {reference vsdbabysoc_1} {layers metal2} {sides 2} {offset 135.6600};
+    {pins OUT[1]} {reference vsdbabysoc_1} {layers metal2} {sides 2} {offset 45.4100};
+    {pins OUT[0]} {reference vsdbabysoc_1} {layers metal3} {sides 1} {offset 221.6200};
+    {pins reset} {reference vsdbabysoc_1} {layers metal3} {sides 1} {offset 133.0000};
+    {pins CLK} {reference vsdbabysoc_1} {layers metal3} {sides 1} {offset 44.3800};
+END PHYSICAL PIN CONSTRAINTS;
+
